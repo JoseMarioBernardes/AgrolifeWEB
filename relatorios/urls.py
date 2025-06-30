7a896ea08b7a57b4259c8de1be95e566c8ec6b9e
Jose Mario <josemariobernardes@gmail.com>
2025-06-30 08:59:03 -0300

    Nova funcionalidade

--- relatorios/urls.py
+++ relatorios/urls.py
@@ -3,4 +3,5 @@ from .views import RelatorioMortalidadeView
 
 urlpatterns = [
     path('mortalidade/', RelatorioMortalidadeView.as_view(), name='relatorio_mortalidade'),
+    path('relatorios/doencas/', RelatorioDoencasView.as_view(), name='relatorio_doencas'),
 ]
